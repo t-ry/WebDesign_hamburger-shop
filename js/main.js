9270929e90020579e584bc24dc798f3167e2ee4e
takahashi.r <takahashi.r299792458@gmail.com>
2022-09-02 07:34:42 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function($) { 
+    //トップ画像切り替え
+    $('.bg-switcher').bgSwitcher({
+        images: ['images/bg1.jpg','images/bg2.jpg','images/bg3.jpg'],
+        interval: 5000,
+        loop: true
+    });
+    $(window).scroll(function(){
+        var targetElement = $('.fadein').offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if ( scroll > targetElement - windowHeight + 200) {
+            $('.fadein').css('opacity','1');
+            $('.fadein').css('transition','translateY(0)')
+        }
+    });
+
+    //スライダー
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        centerMode: true,
+        centerPadding: '20%',
+    });
+
+    //詳細表示
+    $('.more-button').on('click',function() {
+        if($('.open-box').is(':hidden')) {
+            $('.open-box').slideDown();
+            $(this).text('close');
+            $(this).css('background-color','#808080');
+        } else {
+            $('.open-box').slideUp();
+            $(this).text('more');
+            $(this).css('background-color','#fabb51');
+        }
+    });
+
+    //モーダル
+    $(".modal-button").modaal();
+
+    $('nav a[href^="#"]').click(function(){
+        var adjust = 0;
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - adjust;
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+});
